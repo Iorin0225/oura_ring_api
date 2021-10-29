cb5f1813ac7bb78ca5fc9ae1fab6d514ea6c2f99
Iori OSADA <iori.osada@gmail.com>
2021-10-29 10:14:56 +0900

    Initial Commit

---
+++ spec/oura_ring_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OuraRingApi do
+  it "has a version number" do
+    expect(OuraRingApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
