0c0ce0f105adb2f5e827c756b9e4dcfb2fe527fb
Iori OSADA <iori.osada@gmail.com>
2021-10-29 16:50:21 +0900

    feat: implement bed time api

---
+++ lib/oura_ring_api/responses/bedtime.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative "base"
+
+module OuraRingApi
+  module Response
+    class Bedtime < Base
+      def find_by_date(date)
+        date = date.strftime("%Y-%m-%d") if date.respond_to?(:strftime)
+        records.select { |record| record.date == date }
+      end
+
+      class Record < ::OuraRingApi::Response::Base::Record
+        def self.record_key
+          "ideal_bedtimes"
+        end
+
+        KEYS = %w[
+          date
+          bedtime_window
+          status
+        ].freeze
+
+        SUMMARY_DATA_KEYS = KEYS
+        DATE_KEY = "date"
+
+        # I couldn't put this on base class ><
+        KEYS.each do |key|
+          define_method(key) do
+            body[key]
+          end
+        end
+      end
+    end
+  end
+end
